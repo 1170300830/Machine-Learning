71f7a4d871a20c730e3600ac51bc34d273e7b2cc
wongyu1999 <43235704+wongyu1999@users.noreply.github.com>
2018-10-21 10:27:42 +0800

    customer_segmentation

---
+++ Module1/Chapter4/customer_segmentation.py
@@ -0,0 +1,54 @@
+#week 6
+import csv
+
+import numpy as np
+from sklearn import cluster, covariance, manifold
+from sklearn.cluster import MeanShift, estimate_bandwidth
+import matplotlib.pyplot as plt
+
+#加载数据
+input_file = 'wholesale.csv'
+file_reader = csv.reader(open(input_file, 'rt'), delimiter=',')
+X = []
+for count, row in enumerate(file_reader):
+	if not count:
+		names = row[2:]
+		continue
+
+	X.append([float(x) for x in row[2:]])
+
+X = np.array(X)
+
+#估算带宽
+bandwidth = estimate_bandwidth(X, quantile=0.8, n_samples=len(X))
+
+#用平均位移计算聚类
+meanshift_estimator = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+meanshift_estimator.fit(X)
+labels = meanshift_estimator.labels_
+centroids = meanshift_estimator.cluster_centers_
+num_clusters = len(np.unique(labels))
+
+print ("\nNumber of clusters in input data =", num_clusters)
+
+print ("\nCentroids of clusters:")
+print ('\t'.join([name[:3] for name in names]))
+for centroid in centroids:
+	print ('\t'.join([str(int(x)) for x in centroid]))
+
+#可视化数据
+centroids_milk_groceries = centroids[:, 1:3]
+
+#用牛奶供应商的中心点绘制节点
+plt.figure()
+plt.scatter(centroids_milk_groceries[:,0], centroids_milk_groceries[:,1], 
+	s=100, edgecolors='k', facecolors='none')
+
+offset = 0.2
+plt.xlim(centroids_milk_groceries[:,0].min() - offset * centroids_milk_groceries[:,0].ptp(),
+	centroids_milk_groceries[:,0].max() + offset * centroids_milk_groceries[:,0].ptp(),)
+plt.ylim(centroids_milk_groceries[:,1].min() - offset * centroids_milk_groceries[:,1].ptp(),
+	centroids_milk_groceries[:,1].max() + offset * centroids_milk_groceries[:,1].ptp())
+
+plt.title('Centroids of clusters for milk and groceries')
+plt.show()
